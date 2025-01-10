3008095fd24ad32e1e49dff86135b8dde64cbbdb
varsha-v2001 <varshakunnathparambil2019@gmail.com>
2025-01-10 11:10:52 +0530

    quiz v2

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
       <nav className='bg-cyan-700 w-full p-2'>
         <div className='flex justify-between items-center'>
           <a href="" style={{textDecoration:"none"}} className='text-white text-4xl p-2'>Quiz Zone</a>
-          <h4 style={{fontFamily:'"Prata", serif'}} className='text-white text-xl me-5'>About us</h4>
+          <a href='' style={{fontFamily:'"Prata", serif'}} className='text-white text-xl me-5'>About us</a>
         </div>
       </nav>
-      <div className='overflow-y-hidden'>
+      <div className='overflow-y-hidden '>
         <Quiz/>
       </div>
     </>
